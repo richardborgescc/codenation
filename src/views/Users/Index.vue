aa6e0242fd95792c7eab5697d784e0c80aa6eb92
Gabriel Michalski <g.amichalski@gmail.com>
2019-09-04 06:26:21 -0300

    feat(tech): Removido scope e redirecionamento login

--- src/views/Users/Index.vue
+++ src/views/Users/Index.vue
@@ -1,36 +1,15 @@
 <template>
   <div>
     <Navbar>
-      <div class="navbar-end">
+      <div class="navbar-end is-hidden-touch">
         <div class="navbar-item">{{ user.name }}</div>
       </div>
     </Navbar>
     <div class="container">
-      <Bar>
-        <button
-          v-if="hasPermission('users:write')"
-          slot="right"
-          class="button is-primary"
-          @click="showModal"
-        >
-          Novo Usuário
-        </button>
-      </Bar>
       <Table class="is-fullwidth" :columns="columns" :rows="rows">
         <tr slot-scope="row">
-          <td>{{ row.item.id }}</td>
           <td>{{ row.item.name }}</td>
           <td>{{ row.item.email }}</td>
-          <td>
-            <div
-              class="tags has-addons is-marginless"
-              v-for="(scope, index) in row.item.scope"
-              :key="index"
-            >
-              <span :id="`span_${index}`" class="tag">{{ scope.object }}</span>
-              <span class="tag is-primary">{{ scope.action }}</span>
-            </div>
-          </td>
         </tr>
       </Table>
     </div>
@@ -47,34 +26,24 @@ import { domain } from "env";
 
 import Table from "@/components/Table";
 import Navbar from "@/components/Navbar";
-import Bar from "@/components/Bar";
 import Modal from "@/components/Modal";
 
 import Form from "./Form";
 
 export default {
   name: "app",
-  components: { Table, Navbar, Bar, Modal, Form },
+  components: { Table, Navbar, Modal, Form },
   data() {
     return {
       email: "",
       isOpened: false,
-      columns: ["Id", "Nome", "Email", "Permissão"],
+      columns: ["Nome", "Email"],
       rows: []
     };
   },
   async created() {
     const { data } = await axios.get(`${domain}/users`);
-    this.rows = data.map(row => {
-      row.scope = row.scope.map(scope => {
-        const [object, action] = scope.split(":");
-        return {
-          object,
-          action
-        };
-      });
-      return row;
-    });
+    this.rows = data.items;
   },
   computed: {
     ...mapGetters("login", ["user"])
@@ -82,9 +51,6 @@ export default {
   methods: {
     showModal() {
       this.isOpened = true;
-    },
-    hasPermission(scope) {
-      return this.user.scope.includes(scope);
     }
   }
 };
